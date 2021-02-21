6f5765e057040d7ebe7c00438cd10c58eef6accc
Dementev.Aleksander <Dementev.Aleksander@gmail.com>
2021-02-21 15:08:46 +0300

    MiniProjects. Корректировки проекта.

--- 121_good_cheap_fast/script.js
+++ 121_good_cheap_fast/script.js
@@ -10,17 +10,17 @@ function doTheTrick(theClickedOne) {
     if(good.checked && cheap.checked && fast.checked) {
         if(good === theClickedOne) {
             fast.checked = false
-            message.innerHTML = '<p class="green">Качественно и быстро!</p> <img src="smiley.jpg" width="100" alt=";)"/>'
+            message.innerHTML = '<p class="green">Качественно и дёшево, но очень долго!</p> <img src="smiley.jpg" width="100" alt=";)"/>'
         }
 
         if(cheap === theClickedOne) {
             good.checked = false
-            message.innerHTML = '<p class="green">Качественно и быстро!</p> <img src="smiley.jpg" width="100" alt=";)"/>'
+            message.innerHTML = '<p class="green">Дёшево и быстро, но за качество не отвечаю!</p> <img src="smiley.jpg" width="100" alt=";)"/>'
         }
 
         if(fast === theClickedOne) {
             cheap.checked = false
-            message.innerHTML = '<p class="green">Качественно и быстро!</p> <img src="smiley.jpg" width="100" alt=";)"/>'
+            message.innerHTML = '<p class="green">Качественно и быстро, но дорого!</p> <img src="smiley.jpg" width="100" alt=";)"/>'
         }
     } else {
         message.innerHTML = '<p class="red">Выберите варианты!</p>'
